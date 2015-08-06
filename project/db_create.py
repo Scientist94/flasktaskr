271d418c7d92ee2e0aade2d4c7326f626c99077f
Scientist94 <shivam.bhattacharjee94@gmail.com>
2015-08-06 13:35:27 +0530

    user login added

--- project/db_create.py
+++ project/db_create.py
@@ -6,8 +6,8 @@ from datetime import date
 db.create_all()
 
 #insert data
-db.session.add(Task("Finish this project", date(2015, 8, 15), 7, 1))
-db.session.add(Task("Start new project", date(2015, 8, 17), 4, 1))
+# db.session.add(Task("Finish this project", date(2015, 8, 15), 7, 1))
+# db.session.add(Task("Start new project", date(2015, 8, 17), 4, 1))
 
 #commit changes
 db.session.commit()
