8fdf4ccfb147da1c0906896184c4f9dc74746193
Scientist94 <shivam.bhattacharjee94@gmail.com>
2015-08-07 17:46:00 +0530

    db relation and managing sessions done

--- project/views.py
+++ project/views.py
@@ -5,6 +5,7 @@ from functools import wraps
 from flask import Flask, flash, redirect, render_template, request, session, url_for, g
 from forms import AddTaskForm, RegisterForm, LoginForm
 from flask.ext.sqlalchemy import SQLAlchemy 
+import datetime
 
 #config
 app = Flask(__name__)
@@ -29,6 +30,7 @@ def login_required(test):
 @app.route('/logout/')
 def logout():
 	session.pop('logged_in', None)
+	session.pop('user_id', None)
 	flash('Goodbye')
 	return redirect(url_for('login'))
 
@@ -41,6 +43,7 @@ def login():
 			user = User.query.filter_by(name=request.form['name']).first()
 			if user is not None and user.password == request.form['password']:
 				session['logged_in'] = True
+				session['user_id'] = user.id
 				flash('Welcome')
 				return redirect(url_for('tasks'))
 			else:
@@ -77,7 +80,7 @@ def new_task():
 	form = AddTaskForm(request.form)
 	if request.method == 'POST':
 		if form.validate_on_submit():
-			new_task = Task(form.name.data, form.due_date.data, form.priority.data, '1')
+			new_task = Task(form.name.data, form.due_date.data, form.priority.data, datetime.datetime.utcnow(),'1',session['user_id'])
 			db.session.add(new_task)
 			db.session.commit()
 			flash('New entry was successfully posted. Thanks')
